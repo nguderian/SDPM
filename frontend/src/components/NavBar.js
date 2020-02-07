6f78f96c9a8d0fa21040d5af36ba96da65dbcffe
Tanvir Islam <islamtanvir8991@gmail.com>
2020-02-06 20:45:21 -0500

    Decided to use material UI

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,6 +6,14 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -19,6 +27,9 @@ const useStyles = makeStyles(theme => ({
       flexGrow: 1,
       color: 'black'
     },
+    list: {
+        width: 250,
+    },
 }));
 
 const StyledNav = withStyles({
@@ -29,16 +40,58 @@ const StyledNav = withStyles({
 
 const NavBar = () => {
     const classes = useStyles();
+    
+    const [state, setState] = useState({
+        left: false
+    });
+
+    const toggleDrawer = (side, open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key == 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [side]: open });
+    };
+
+    const sideList = side => (
+        <div
+          className={classes.list}
+          role="presentation"
+          onClick={toggleDrawer(side, false)}
+          onKeyDown={toggleDrawer(side, false)}
+        >
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+    );
 
     return (
         <Fragment>
             <StyledNav position="static" classname={classes.root}>
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="black" aria-label="menu">
+                    <IconButton edge="start" className={classes.menuButton} color="black" aria-label="menu" onClick={toggleDrawer('left', true)}>
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h4" className={classes.title}>
-                        News
+                    <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
+                        {sideList('left')}
+                    </Drawer>
+                    <Typography variant="h6" className={classes.title}>
+                        Senior Design Project Manager
                     </Typography>
                     <Button color="black">Logout</Button>
                 </Toolbar>
