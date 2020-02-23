d676962bed064421da2c05108218462332cce152
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2020-02-23 15:21:08 -0500

    Initial

---
+++ canvas/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const session = require('express-session');
+const lti = require('./lti');
+
+const port = process.env.PORT || 3002;
+// this express server should be secured/hardened for production use
+const app = express();
+
+app.set('view engine', 'pug');
+// memory store shouldn't be used in production
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'dev',
+  resave: false,
+  saveUninitialized: true,
+}));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set('json spaces', 2);
+
+app.enable('trust proxy');
+
+app.get('/', (req, res, next) => {
+  return res.send({status: 'Up'});
+});
+
+app.get('/application', (req, res, next) => {
+  if (req.session.userId) {
+    console.log(req.session.userId);
+    console.log(req.session.coursename);
+    return res.render('newpage', {
+      email: req.session.email,
+      username: req.session.username,
+      ltiConsumer: req.session.ltiConsumer,
+      userId: req.session.userId,
+      isTutor: req.session.isTutor,
+      context_id: req.session.context_id
+    })
+  } else {
+    next(new Error('Session invalid. Please login via LTI to use this application.'));
+  }
+});
+
+app.post('/launch_lti', lti.handleLaunch);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ canvas/lti/index.js
@@ -0,0 +1,67 @@
+'use strict';
+const lti = require('ims-lti');
+// MemoryStore shouldn't be used in production. Timestamps must be valid within a 5 minute grace period.
+const nonceStore = new lti.Stores.MemoryStore();
+// secrets should be stored securely in a production app
+const secrets = {
+  demo: 'xzc342AScx',
+  demo2: 'dh43fvL-ew',
+  canvaskey: 'canvassecret'
+};
+
+const getSecret = (consumerKey, callback) => {
+  const secret = secrets[consumerKey];
+  if (secret) {
+    return callback(null, secret);
+  }
+
+  let err = new Error(`Unknown consumer ${consumerKey}`);
+  err.status = 403;
+
+  return callback(err);
+};
+
+exports.handleLaunch = (req, res, next) => {
+  if (!req.body) {
+    let err = new Error('Expected a body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const consumerKey = req.body.oauth_consumer_key;
+  if (!consumerKey) {
+    let err = new Error('Expected a consumer');
+    err.status = 422;
+    return next(err);
+  }
+
+  getSecret(consumerKey, (err, consumerSecret) => {
+    if (err) {
+      return next(err);
+    }
+
+    const provider = new lti.Provider(consumerKey, consumerSecret, nonceStore, lti.HMAC_SHA1);
+
+    provider.valid_request(req, (err, isValid) => {
+      if (err) {
+        return next(err);
+      }
+      if (isValid) {
+        req.session.regenerate(err => {
+          if (err) next(err);
+          req.session.email = provider.body.lis_person_contact_email_primary;
+          req.session.contextId = provider.context_id;
+          req.session.userId = provider.userId;
+          req.session.username = provider.body.lis_person_name_full;
+          req.session.ltiConsumer = provider.body.tool_consumer_instance_guid;
+          req.session.isTutor = provider.instructor === true;
+          req.session.context_id = provider.context_id;
+          req.session.coursename = provider.context_title;
+          return res.redirect(301, '/application');
+        });
+      } else {
+        return next(err);
+      }
+    });
+  });
+};
