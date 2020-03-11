d48078a645c0ae9e5fa438cd04489ee65267028d
Tanvir Islam <islamtanvir8991@gmail.com>
2020-03-11 11:45:10 -0400

    working on answering questions

--- frontend/src/components/forms/Assignment.js
+++ frontend/src/components/forms/Assignment.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Assignment = ({ match, location }) => {
+    let arr = [];
     const { form } = location.state;
     
+    form.questions.map((question, index) => {
+        let obj = {
+            questionID = question.questionId,
+            answer = []
+        };
+        arr.push(obj);
+    });
+
+    const [answers, setAnswers] = useState(arr);
+
     return(
         <div>
             <h1>{form.id}</h1>
             <h2>{form.formTitle}</h2>
+
+            {form.questions.map((question, index) => {
+                {question.questionType === 'Free Response' && <TakeFreeResponse />}
+                {question.questionType === 'Multiple Choice' && <TakeMultipleChoice />}
+                {question.questionType === 'Likert' && <TakeLikert />}
+            })}
         </div>
         
     );
--- frontend/src/components/forms/questions/EditFreeResponse.js
+++ frontend/src/components/forms/questions/EditFreeResponse.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { FormControl, FormGroup, FormControlLabel, Checkbox, TextField } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
