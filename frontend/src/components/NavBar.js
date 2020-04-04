0830a8fd832cca63bf644ecefb9dc64e1dd6d1ec
Tanvir Islam <islamtanvir8991@gmail.com>
2020-04-04 18:50:42 -0400

    Routing Changes

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -106,19 +106,15 @@ const NavBar = ({ user_id, userType, token, loggedIn }) => {
                 <ListItemIcon><ClassIcon/></ListItemIcon>
                 <ListItemText primary='Class'/>
               </ListItem>
-              <ListItem button component={Link} to='/'>
-                <ListItemIcon><AssignmentIcon/></ListItemIcon>
-                <ListItemText primary='Assignments'/>
-              </ListItem>
-              <ListItem button component={Link} to='/coordinator/CreateSurvey'>
+              <ListItem button component={Link} to='/coordinator/Survey/CreateSurvey'>
                 <ListItemIcon><CreateIcon/></ListItemIcon>
                 <ListItemText primary='Create Peer Review'/>
               </ListItem>
-              <ListItem button component={Link} to='/coordinator/CreateQuiz'>
+              <ListItem button component={Link} to='/coordinator/Quiz/CreateQuiz'>
                 <ListItemIcon><CreateIcon/></ListItemIcon>
                 <ListItemText primary='Create Quiz'/>
               </ListItem>
-              <ListItem button component={Link} to='/meeting/CreateMeeting'>
+              <ListItem button component={Link} to='/student/Meeting/CreateMeeting'>
                 <ListItemIcon><CreateIcon/></ListItemIcon>
                 <ListItemText primary='Create Meeting'/>
               </ListItem>
