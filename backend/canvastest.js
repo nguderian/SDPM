a05fa3993088a22f0d8cd54bc8c30bbdc9067306
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2020-04-16 23:11:08 -0400

    modified to get some pictures for the final doc

--- backend/canvastest.js
+++ backend/canvastest.js
@@ -5,14 +5,13 @@ function callback(result){
     index = 0;
     console.log('There are ' + buffer.length + ' elements.');
     while(index < buffer.length){
-        //console.log(buffer[index]);
-        console.log('ID:' + buffer[index].id + '    NAME:' + buffer[index].name);
+        console.log(buffer[index]);
+        //console.log('ID:' + buffer[index].id + '    NAME:' + buffer[index].name);
         index++;
     };
 }
 
-canvas.getCoursesofUser(callback, 35); //users: 35, 40, 36, 39
-canvas.getStudentsinCourse(callback, 1); //courses: 1, 2
-canvas.getGroupsinCourse(callback, 1); //courses: 1, 2
-canvas.getUsersinGroup(callback, 1); // groups: 1, 2
-canvas.getUsersinGroup(callback, 2);
+//canvas.getCoursesofUser(callback, 35); //users: 35, 40, 36, 39
+//canvas.getStudentsinCourse(callback, 1); //courses: 1, 2
+//canvas.getGroupsinCourse(callback, 1); //courses: 1, 2
+canvas.getUsersinGroup(callback, 2); // groups: 1, 2
